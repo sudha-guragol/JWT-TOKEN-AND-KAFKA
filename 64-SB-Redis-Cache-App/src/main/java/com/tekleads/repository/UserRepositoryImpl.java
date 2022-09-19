af5e978433d0f73b1334c5b49811a535e7b477bc
sudha-guragol <110382111+sudha-guragol@users.noreply.github.com>
2022-09-19 18:34:30 +0530

    Redis Cache

---
+++ 64-SB-Redis-Cache-App/src/main/java/com/tekleads/repository/UserRepositoryImpl.java
@@ -0,0 +1,45 @@
+package com.tekleads.repository;
+
+import java.util.Map;
+
+import org.springframework.data.redis.core.HashOperations;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Repository;
+
+import com.tekleads.model.User;
+
+@Repository
+public class UserRepositoryImpl implements UserRepository {
+
+	private HashOperations<String, String, User> hashOperations;
+
+	public UserRepositoryImpl(RedisTemplate<String, User> redisTemplate) {
+		hashOperations = redisTemplate.opsForHash();
+	}
+
+	@Override
+	public void save(User user) {
+		hashOperations.put("USER", user.getId(), user);
+	}
+
+	@Override
+	public Map<String, User> findAll() {
+		return hashOperations.entries("USER");
+	}
+
+	@Override
+	public User findById(String id) {
+		User user = (User) hashOperations.get("USER", id);
+		return user;
+	}
+
+	@Override
+	public void update(User user) {
+		save(user);
+	}
+
+	@Override
+	public void delete(String id) {
+		hashOperations.delete("USER", id);
+	}
+}
